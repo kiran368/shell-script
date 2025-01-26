3d9b40add829235f79c35f8ac123c9db392ad013
kiran kumar <kirankumar.bk20@gmail.com>
2025-01-26 09:34:42 +0530

    hello world sample shell

---
+++ 01variable.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#sample conversation without variables
+
+echo "rakesh: hi rahul"
+echo "rahul: hi rakesh how are you doing"
+echo "rakesh: i am fine how about you"
+echo "rahul: i am doing well"
