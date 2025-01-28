f0bbf9c97d5d89fa5d4740e9d075c4d962de037a
kiran kumar <kirankumar.bk20@gmail.com>
2025-01-28 08:14:43 +0530

    data commit

--- 04variable-read.bash
+++ 04variable-read.bash
@@ -2,8 +2,8 @@
 #read running arguments and hide input
 echo "please enter username : "
 # reading inputs without hide
-read " USERNAME "
-echo " username is : ${USERNAME} "
-echo " please enter your password : "
-read " PWD "
-echo " ${PWD} "
+read "USERNAME"
+echo "username is : ${USERNAME}"
+echo "please enter your password :"
+read "PWD"
+echo "${PWD}"
