2f840e9680f756d44b8613aa05e4f5b0ce23ce86
kiran kumar <kirankumar.bk20@gmail.com>
2025-01-28 08:50:46 +0530

    data commit

--- 05data-types.sh
+++ 05data-types.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #data types example
-NUM1=$1
-NUM2=$2
+echo enter NUM1
+read NUM1
+echo enter NUM2
+read NUM2
 TIMESTAMP=$(date)
 SUM=$(($NUM1+$NUM2))
 echo "sum of 2 numbers $NUM1 and $NUM2 is ${TIMESTAMP} : ${SUM}"
