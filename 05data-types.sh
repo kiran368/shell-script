fac304b62df9e9f9f25a67cf870979cf008e65cb
kiran kumar <kirankumar.bk20@gmail.com>
2025-01-28 08:46:01 +0530

    data commit

--- 05data-types.sh
+++ 05data-types.sh
@@ -3,5 +3,5 @@
 NUM1=$1
 NUM2=$2
 TIMESTAMP=$(date)
-SUM=$($NUM1+$NUM2)
-echo "sum of 2 numbers is ${TIMESTAMP} : ${SUM}"
+SUM=$(($NUM1+$NUM2))
+echo "sum of 2 numbers $NUM1 and $NUM2 is ${TIMESTAMP} : ${SUM}"
